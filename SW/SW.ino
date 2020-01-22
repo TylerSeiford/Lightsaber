8cf9c46bf40d33ce35ac112bbff33b8edae6b7c9
TylerSeiford <TylerSeiford@users.noreply.github.com>
2020-01-22 11:11:11 -0600

    Working code with a toggle switchbutton

--- SW/SW.ino
+++ SW/SW.ino
@@ -1,14 +1,15 @@
 #include <FastLED.h>
 
-constexpr uint8_t ARGB_PIN = 4;
 constexpr uint8_t BTN_PIN = 3;
-
-constexpr uint8_t NUM_LEDS = 30;
-constexpr uint8_t NUM_MODES = 10;
+constexpr uint8_t NUM_MODES = 6;
 constexpr uint8_t DELAY = 20;
 
+constexpr uint8_t ARGB_PIN = 4;
+constexpr uint8_t NUM_LEDS = 30;
+
 CRGB leds[NUM_LEDS];
-uint8_t mode = 0;
+uint8_t mode = NUM_MODES - 1;
+uint8_t lastState = HIGH;
 
 void setup() {
 	FastLED.addLeds<NEOPIXEL, ARGB_PIN>(leds, NUM_LEDS);
@@ -18,56 +19,78 @@ void setup() {
 		leds[i] = CRGB::Black;
 	}
 	FastLED.show();
+  Serial.begin(9600);
 }
 
 void loop() {
+	Serial.print("Read: ");
 	if (digitalRead(BTN_PIN) == LOW) {
-		mode = (mode + 1) % NUM_MODES;
+		Serial.print("L");
+		if (lastState == HIGH) {
+			lastState = LOW;
+			mode = (mode + 1) % NUM_MODES;
+			Serial.print("->H: ");
+			Serial.println(mode);
 
-		switch (mode) {
-		case 1:
-			for (int i = 0; i < NUM_LEDS; i++) {
-				leds[i] = CRGB::Blue;
-				FastLED.show();
-				delay(DELAY);
+			switch (mode) {
+			case 0:
+				for (int i = 0; i < NUM_LEDS; i++) {
+					leds[i] = CRGB::Blue;
+					FastLED.show();
+					delay(DELAY);
+				}
+				break;
+			case 1:
+				for (int i = 0; i < NUM_LEDS; i++) {
+					leds[i] = CRGB::Green;
+					FastLED.show();
+					delay(DELAY);
+				}
+				break;
+			case 2:
+				for (int i = 0; i < NUM_LEDS; i++) {
+					leds[i] = CRGB::Red;
+					FastLED.show();
+					delay(DELAY);
+				}
+				break;
+			case 3:
+				for (int i = 0; i < NUM_LEDS; i++) {
+					leds[i] = CRGB::Purple;
+					FastLED.show();
+					delay(DELAY);
+				}
+				break;
+			case 4:
+				for (int i = 0; i < NUM_LEDS; i++) {
+					leds[i] = CRGB::Yellow;
+					FastLED.show();
+					delay(DELAY);
+				}
+				break;
+			case 5:
+				for (int i = 0; i < NUM_LEDS; i++) {
+					leds[i] = CRGB::White;
+					FastLED.show();
+					delay(DELAY);
+				}
+				break;
+			default:
+				delay(DELAY * NUM_LEDS);
+				break;
 			}
-			break;
-		case 3:
-			for (int i = 0; i < NUM_LEDS; i++) {
-				leds[i] = CRGB::Green;
-				FastLED.show();
-				delay(DELAY);
-			}
-			break;
-		case 5:
-			for (int i = 0; i < NUM_LEDS; i++) {
-				leds[i] = CRGB::Purple;
-				FastLED.show();
-				delay(DELAY);
-			}
-			break;
-		case 7:
-			for (int i = 0; i < NUM_LEDS; i++) {
-				leds[i] = CRGB::Yellow;
-				FastLED.show();
-				delay(DELAY);
-			}
-			break;
-		case 9:
-			for (int i = 0; i < NUM_LEDS; i++) {
-				leds[i] = CRGB::White;
-				FastLED.show();
-				delay(DELAY);
-			}
-			break;
-		default:
-			for (int i = NUM_LEDS - 1; i >= 0; i--) {
-				leds[i] = CRGB::Black;
-				delay(DELAY);
-				FastLED.show();
-			}
-			break;
 		}
 	}
-	delay(1);
+	else {
+		Serial.print("H");
+		if (lastState == LOW) {
+			Serial.println("->L");
+			lastState = HIGH;
+			for (int i = NUM_LEDS - 1; i >= 0; i--) {
+				leds[i] = CRGB::Black;
+				FastLED.show();
+				delay(DELAY);
+			}
+		}
+	}
 }
