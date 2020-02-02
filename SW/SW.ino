8dc1496565e5ab642496c4f9133a254d27f2d351
TylerSeiford <TylerSeiford@users.noreply.github.com>
2020-02-02 15:23:18 -0600

    Small updates

--- SW/SW.ino
+++ SW/SW.ino
@@ -1,15 +1,17 @@
 #include <FastLED.h>
 
+constexpr uint8_t ARGB_PIN = 4;
+constexpr uint8_t NUM_LEDS = 144;
+
 constexpr uint8_t BTN_PIN = 3;
 constexpr uint8_t NUM_MODES = 6;
-constexpr uint8_t DELAY = 10;
-
-constexpr uint8_t ARGB_PIN = 4;
-constexpr uint8_t NUM_LEDS = 60;
+constexpr uint8_t DELAY = (600 / NUM_LEDS);
 
 CRGB leds[NUM_LEDS];
 uint8_t mode = NUM_MODES - 1;
 uint8_t lastState = HIGH;
+
+//constexpr uint8_t BRIGHTNESS =  ((60 * 255) / NUM_LEDS);
 
 void setup() {
 	FastLED.addLeds<NEOPIXEL, ARGB_PIN>(leds, NUM_LEDS);
@@ -18,6 +20,7 @@ void setup() {
 	for (int i = 0; i < NUM_LEDS; i++) {
 		leds[i] = CRGB::Black;
 	}
+	//FastLED.setBrightness(BRIGHTNESS);
 	FastLED.show();
 	//Serial.begin(9600);
 }
@@ -37,46 +40,46 @@ void loop() {
 				for (int i = 0; i < NUM_LEDS; i++) {
 					leds[i] = CRGB::Blue;
 					FastLED.show();
-					delay(DELAY);
+					FastLED.delay(DELAY);
 				}
 				break;
 			case 1:
 				for (int i = 0; i < NUM_LEDS; i++) {
 					leds[i] = CRGB::Green;
 					FastLED.show();
-					delay(DELAY);
+					FastLED.delay(DELAY);
 				}
 				break;
 			case 2:
 				for (int i = 0; i < NUM_LEDS; i++) {
 					leds[i] = CRGB::Red;
 					FastLED.show();
-					delay(DELAY);
+					FastLED.delay(DELAY);
 				}
 				break;
 			case 3:
 				for (int i = 0; i < NUM_LEDS; i++) {
 					leds[i] = CRGB::Purple;
 					FastLED.show();
-					delay(DELAY);
+					FastLED.delay(DELAY);
 				}
 				break;
 			case 4:
 				for (int i = 0; i < NUM_LEDS; i++) {
 					leds[i] = CRGB::Yellow;
 					FastLED.show();
-					delay(DELAY);
+					FastLED.delay(DELAY);
 				}
 				break;
 			case 5:
 				for (int i = 0; i < NUM_LEDS; i++) {
 					leds[i] = CRGB::White;
 					FastLED.show();
-					delay(DELAY);
+					FastLED.delay(DELAY);
 				}
 				break;
 			default:
-				delay(DELAY * NUM_LEDS);
+				FastLED.delay(DELAY * NUM_LEDS);
 				break;
 			}
 		}
@@ -89,7 +92,7 @@ void loop() {
 			for (int i = NUM_LEDS - 1; i >= 0; i--) {
 				leds[i] = CRGB::Black;
 				FastLED.show();
-				delay(DELAY);
+				FastLED.delay(DELAY);
 			}
 		}
 	}
